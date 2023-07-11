132dd172dca0a257e886d11a5f241aae83720619
Marianna Grzesik <gusmanmarianna@gmail.com>
2023-07-11 21:31:03 +0200

    v 0.2

--- my-app/src/components/Home/Home.js
+++ my-app/src/components/Home/Home.js
@@ -1,7 +1,52 @@
+import Grid from '@mui/material/Grid';
+import { Paper, Box } from '@mui/material';
+
 export default function Home() {
-    return (
-      <div >
-        <h1>Home</h1>
-      </div>
-    );
-  }
+	return (
+		<>
+			<Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }} >
+      <Grid item xs={12} style={{marginTop: '20px', marginBottom: '20px', textAlign:'center'}}>
+					HOME
+				</Grid>
+				<Grid item xs={3}>
+        <Box
+      sx={{
+        display: 'flex',
+        flexWrap: 'wrap',
+        '& > :not(style)': {
+          m: 1,
+          width: 128,
+          height: 128,
+        },
+      }}
+    >
+      
+      <Paper elevation={3} />
+    </Box>
+				</Grid>
+				<Grid item xs={3}>
+					2
+				</Grid>
+				<Grid item xs={3}>
+					3
+				</Grid>
+				<Grid item xs={3}>
+					4
+				</Grid>
+        <Grid item xs={3}>
+					5
+				</Grid>
+				<Grid item xs={3}>
+					6
+				</Grid>
+				<Grid item xs={3}>
+					7
+				</Grid>
+				<Grid item xs={3}>
+					8
+				</Grid>
+			</Grid>
+			
+		</>
+	);
+}
